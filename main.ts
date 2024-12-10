48dde5cc632f48afbdf3ae2976315d369c708c57
Learn STEM by Doing <32452061+eesilas@users.noreply.github.com>
2024-12-10 11:31:39 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,13 @@
+Obloq.Obloq_mqtt_setup(
+SerialPin.P2,
+SerialPin.P1,
+"yourSSID",
+"yourPASSWORD",
+"yourIotId",
+"yourIotPwd",
+"yourIotTopic",
+Obloq.SERVERS.China
+)
+basic.forever(function () {
+	
+})
